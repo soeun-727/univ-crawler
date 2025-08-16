934f05246ad65117056d743eb62c754522258826
soeun-727 <soeun2215562@sm.ac.kr>
2025-08-16 20:51:28 +0900

    request

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,6 +1,9 @@
 //src/crawler.rs
 use crate::schools::sookmyung;
-//다른 학교 모듈 추가 예정
+
+//여기는 sookmyung.rs랑은 호환이 되는데
+//seoul.rs랑 dongduk.rs 경우 notices -> fetch_notices로 안돼있는 경우 아래 식이 작동을 안할 거 같아서
+//아래 코드랑 같이 작동하는지 각 학교 코드확인해주시고 안되면은 각 학교 코드 수정해주시면 될 것 같습니다! 
 
 pub fn crawl(school_name: Option<&str>) -> Result<Vec<sookmyung::Notice>, Box<dyn std::error::Error>> {
     match school_name {
@@ -26,9 +29,9 @@ pub fn crawl(school_name: Option<&str>) -> Result<Vec<sookmyung::Notice>, Box<dy
         None => {
             println!("<<전체 학교 공지사항>>");
             let notices = sookmyung::fetch_notices()?;
+            let notices = dongduk::fetch_notices()?;
+            let notices = seoul::fetch_notices()?;
             Ok(notices)
-            //let notices = dongduk::fetch_notices()?;
-            //let notices = seoul::fetch_notices()?;
         }
     }
 }
